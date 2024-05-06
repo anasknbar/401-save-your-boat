6d48ce9077fae10659c4aa76b388d183551bd522
anasknbar <knbaranasgithub@gmail.com>
2024-05-06 17:30:23 +0000

    lab-01 has been pushed

---
+++ word_date.py
@@ -0,0 +1,19 @@
+words = [
+   
+   {
+     "word" : "banana",
+      "hints": ["It's a long, curved fruit.", "It's yellow when ripe.", "It's a good source of potassium.", "It's often used in smoothies.", "It's commonly found in tropical regions."]
+   },
+   {
+      "word":"carrot",
+      "hints": ["It's a root vegetable.", "It's orange in color.", "It's crunchy when eaten raw.", "It's rich in beta-carotene.", "It's often used in salads and soups."],
+   },
+   {
+      "word":"dog",
+       "hints": ["It's a common household pet.", "It's known for its loyalty.", "It's often referred to as 'man's best friend.'", "It comes in various breeds and sizes.", "It enjoys playing fetch."],
+   },
+   {
+      "word":"copmuter",
+      "hints": ["It's an electronic device.", "It's used for processing data.", "It consists of hardware and software components.", "It can perform calculations and store information.", "It's commonly used for work and entertainment."]
+   }
+]
